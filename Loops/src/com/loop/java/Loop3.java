2ec80fa8a7215e4d172b4d28e278fb7f747b8c6b
Himanshu25kapure <kapurehs@gmail.com>
2025-07-09 12:44:20 +0530

    loop 3

---
+++ Loops/src/com/loop/java/Loop3.java
@@ -0,0 +1,13 @@
+package com.loop.java;
+
+public class Loop3 {
+
+	public static void main(String[] args) {
+		for (;;) {
+			System.out.print("Hello World");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
