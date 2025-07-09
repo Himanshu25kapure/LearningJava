81bbf98498f47ce6c2d32c9b52748a50931a0e10
Himanshu25kapure <kapurehs@gmail.com>
2025-07-09 12:40:26 +0530

    loop2

---
+++ Loops/src/com/loop/java/Loop2.java
@@ -0,0 +1,13 @@
+package com.loop.java;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		for (int i=5 ; i>0 ; i--) {
+			System.out.println(i);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
